70238beb153f54d3f81d46eafe8b9e85c0b88800
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2024-08-22 12:16:03 +0530

    first commit

---
+++ loginpage/app1/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path,include
+from .views import authview,home
+from . import views
+
+urlpatterns = [
+    path ('home/',home,name='home'),
+    path('',authview,name='authview'),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path('logout/', views.custom_logout, name='logout')
+
+]
