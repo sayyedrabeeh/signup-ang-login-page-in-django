94c9be6a50d420ab3714f5217a8eb8a5950fdae7
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-11-11 17:11:39 +0530

    allowed hosts

--- signup-ang-login-page-in-django-save/loginpage/loginpage/settings.py
+++ signup-ang-login-page-in-django-save/loginpage/loginpage/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-s!8e#f$qdw$%ax!xut$e6+n=zks&y!ps97ipmte)i-n@wv^!h#
 # ARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rabeeh.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
